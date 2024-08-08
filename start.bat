6e8ac9de6b8c04a00d9185cefd9ea6ebda866c66
xxcxatlove <linalabelle2000@gmail.com>
2024-08-08 17:51:31 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 title GalackInfoGrab by GalackQSM#7926
-set token=
+set token=askipjedoisxhangerdoncazz
 
 set /p token=Entrez le token d'un membre ou bot: %=%
 cls
 
-node "info-min.js" %token%
+node "info-min.js" %askipjedoisxhangerdoncazz%
 pause > nul
